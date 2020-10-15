5495a98678a6379721f06dc1d6346db884485017
sbobhuang <github@bobhuang.xyz>
2020-10-15 12:32:36 +0800

    导出zip修改

--- app/main/export.py
+++ app/main/export.py
@@ -1,8 +1,9 @@
+import os
+
 from flask import send_file
+from flask_moment import datetime
 
 from . import main
-import os
-from datetime import datetime
 from ..fuc import make_zip
 
 basedir = os.path.abspath(os.path.dirname(__file__))  # 获取当前项目的绝对路径
@@ -11,7 +12,8 @@ basedir = os.path.join(basedir, "../../")
 
 @main.route('/export', methods=['GET', 'POST'])
 def exportAllHomeWork():
-    save_filename = '学生作业' + datetime.utcnow().strftime('%m-%d %H:%M:%S') + '.zip'
-    save_filename = os.path.join(basedir, 'Download', save_filename)
+    save_filename1 = '作业导出' + datetime.now().strftime('%m-%d_%H_%M_%S') + '.zip'
+    save_filename = os.path.join(basedir, 'Download', save_filename1)
     make_zip(os.path.join(basedir, 'ZY'), save_filename)
-    return send_file(save_filename, as_attachment=True)
+    print(save_filename1)
+    return send_file(save_filename, as_attachment=True, attachment_filename=save_filename1)
